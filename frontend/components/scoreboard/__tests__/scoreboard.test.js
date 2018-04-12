92afd28ff6ebfa1a9365895f0fa55f61c36ff997
Omar <omar.dlsg@gmail.com>
2018-04-12 16:35:27 -0700

    Write first Scoreboard tests

--- frontend/components/scoreboard/__tests__/scoreboard.test.js
+++ frontend/components/scoreboard/__tests__/scoreboard.test.js
@@ -2,13 +2,21 @@ import Scoreboard from '../Scoreboard.jsx';
 import React from 'react';
 import { mount } from 'enzyme';
 
-test('Scoreboard component begins with 0 for player and AI', () => {
-    const wrapper = mount(
-        <Scoreboard />
-    );
+describe('Scoreboard component', () => {
+    describe('tracking scores', () => {
+        const scoreboardWrapper = mount(
+            <Scoreboard />
+        );
 
-    const pScore = wrapper.find('.player-score');
-    const AIScore = wrapper.find('.ai-score');
-    expect(pScore.text()).toBe('0');
-    expect(AIScore.text()).toBe('0');
+        const aiScore = scoreboardWrapper.find('.AI-score');
+        const playerScore = scoreboardWrapper.find('.player-score');
+
+        test('AI begins with a score of 0', () => {
+            expect(playerScore.text()).toBe('0');
+        });
+
+        test('player begins with a score of 0', () => {
+            expect(aiScore.text()).toBe('0');
+        });
+    });
 });
