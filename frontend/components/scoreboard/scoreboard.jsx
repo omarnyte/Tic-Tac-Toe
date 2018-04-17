6be920b69a343610caa189d23516d6bd2e164f1d
Omar <omar.dlsg@gmail.com>
2018-04-16 18:25:45 -0700

    Turn ScoreBoard into a functional component

--- frontend/components/scoreboard/scoreboard.jsx
+++ frontend/components/scoreboard/scoreboard.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 
-export default class Scoreboard extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="scoreboard-div">
-                <div className="player-score-div">
-                    <span>Player:</span>
-                    <span className="player-score">{this.props.humanScore}</span>
-                </div>
-                <div className="AI-score-div">
-                    <span>Computer:</span>
-                    <span className="AI-score">{this.props.AIScore}</span>
-                </div>
+export default function ScoreBoard(props) {
+    return (
+        <div className="scoreboard-div">
+            <div className="player-score-div">
+                <span>Player:</span>
+                <span className="player-score">{ props.humanScore }</span>
             </div>
-        )
-    }
-
+            <div className="AI-score-div">
+                <span>Computer:</span>
+                <span className="AI-score">{ props.AIScore }</span>
+            </div>
+        </div>
+    );
 } 
