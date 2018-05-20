285170a5361d5a96d7c8c6054792dda6cb7526af
Omar De Los Santos <omar.dlsg@gmail.com>
2018-05-20 10:41:57 -0700

    Fix lint errors on app.jsx

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Game from './components/game/game.jsx';
+import Game from './components/game/game';
 
+// eslint-disable-next-line no-undef
 ReactDOM.render(<Game />, document.getElementById('root'));
